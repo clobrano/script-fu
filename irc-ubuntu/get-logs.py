3bda5c899acfb6a58fa5cdb853056eee47c4d8bb
Carlo Lobrano <c.lobrano@gmail.com>
2017-08-24 09:20:56 +0200

    codereview: fixed PEP8 errors

--- irc-ubuntu/get-logs.py
+++ irc-ubuntu/get-logs.py
@@ -4,39 +4,38 @@
 '''
 Get Ubuntu IRC logs
 
-usage: get-logs.py [--date=date] [--channel=channel]
+usage: get-logs.py [--DATE=DATE] [--channel=channel]
 
 options:
     -c channel, --channel channel   The channel whose logs you want to get [default: ubuntu-desktop]
-    -d date, --date=date            The date of the logs in format YYYY-MM-DD [default: today]
+    -d DATE, --DATE=DATE            The DATE of the logs in format YYYY-MM-DD [default: today]
 '''
-import sys
-import docopt
+
 import urllib2
 import datetime
+import docopt
 
-opts = docopt.docopt (__doc__)
+OPTS = docopt.docopt(__doc__)
 
-date = opts['--date']
-if date == 'today':
-    date = str (datetime.datetime.now ()).replace ('-', '/').split (' ')[0]
+DATE = OPTS['--DATE']
+if DATE == 'today':
+    DATE = str(datetime.datetime.now()).replace('-', '/').split(' ')[0]
 else:
-    date = date.replace ('-', '/')
+    DATE = DATE.replace('-', '/')
 
-channel = opts ['--channel']
+CHANNEL = OPTS['--channel']
+URL = 'https://irclogs.ubuntu.com/{DATE}/%23{channel}.txt'.format(DATE=DATE, channel=CHANNEL)
+ALERT = False
+MESSAGES = []
 
-url = 'https://irclogs.ubuntu.com/{date}/%23{channel}.txt'.format (date=date, channel=channel)
-
-alert = False
-messages = []
-for line in urllib2.urlopen(url):
+for line in urllib2.urlopen(URL):
     line = line.rstrip()
     if "lobrano" in line or "carlo" in line:
         alert = True
-        messages.append (line)
-    print (line)
+        MESSAGES.append(line)
+    print line
 
 if alert:
-    print ("\nThere are messages for you");
-    for msg in messages:
-        print (msg)
+    print "\nThere are messages for you"
+    for msg in MESSAGES:
+        print msg
