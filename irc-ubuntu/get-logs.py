289f70075ade06b65b1d38dacf61596793a59b30
Carlo Lobrano <c.lobrano@gmail.com>
2017-08-24 09:29:50 +0200

    color: integrated Raffaello in get-logs and removed irc color file

--- irc-ubuntu/get-logs.py
+++ irc-ubuntu/get-logs.py
@@ -14,15 +14,39 @@ options:
 import urllib2
 import datetime
 import docopt
+try:
+    from raffaello import Raffaello, parse_request
+
+    REQUEST = r'''
+\d+:\d+=>cyan_bold
+flexiondotorg=>color007_bold
+Laney=>color049_bold
+jbicha=>color230_bold
+seb128=>yellow_bold
+Trevinho=>color120_bold
+tsimonq2=>color117_bold
+willcooke=>color122_bold
+hikiko=>bgcolor019_bold
+didrocks=>color111_bold
+duflu=>color229_bold
+oSoMoN=>color123_bold
+kenvandine=>color210_bold
+n-m=>yellow_bold
+network\smanager=>yellow_bold
+m-m=>yellow_bold
+clobrano=>green_bold
+'''
+    RAFFAELLO = Raffaello(parse_request(REQUEST))
+except ImportError as error:
+    print 'could not colorize output: {}'.format(error)
+    RAFFAELLO = None
 
 OPTS = docopt.docopt(__doc__)
-
 DATE = OPTS['--DATE']
 if DATE == 'today':
     DATE = str(datetime.datetime.now()).replace('-', '/').split(' ')[0]
 else:
     DATE = DATE.replace('-', '/')
-
 CHANNEL = OPTS['--channel']
 URL = 'https://irclogs.ubuntu.com/{DATE}/%23{channel}.txt'.format(DATE=DATE, channel=CHANNEL)
 ALERT = False
@@ -31,11 +55,17 @@ MESSAGES = []
 for line in urllib2.urlopen(URL):
     line = line.rstrip()
     if "lobrano" in line or "carlo" in line:
-        alert = True
+        ALERT = True
         MESSAGES.append(line)
-    print line
+    if RAFFAELLO:
+        print RAFFAELLO.paint(line)
+    else:
+        print line
 
-if alert:
+if ALERT:
     print "\nThere are messages for you"
     for msg in MESSAGES:
-        print msg
+        if RAFFAELLO:
+            print RAFFAELLO.paint(msg)
+        else:
+            print msg
