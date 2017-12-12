d751ba5748f90650d1571da22b9618efb8c49409
Carlo Lobrano <c.lobrano@gmail.com>
2017-12-12 17:24:06 +0100

    reduced delay between theme toggle

--- gnome-toggle-communitheme.sh
+++ gnome-toggle-communitheme.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita
-sleep 2
+sleep 1
 gsettings set org.gnome.desktop.interface gtk-theme Communitheme
