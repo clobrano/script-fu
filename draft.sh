4eae6192602b6ca333636a15e89b728d06762249
Carlo Lobrano <c.lobrano@gmail.com>
2018-12-05 12:31:20 +0100

    added new script draft

---
+++ draft.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+## Simple and lazy script to write NEOVIM draft in $HOME/.drafts folder
+set -e
+NAME=$1
+
+[ ! -d ${HOME}/.drafts ] && mkdir ${HOME}/.drafts
+nvim -c "Writer" ${HOME}/.drafts/$NAME.md
+
