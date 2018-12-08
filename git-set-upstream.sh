6f614d40ce01b01e3415adde71dc54191ea9ad5b
Carlo Lobrano <c.lobrano@gmail.com>
2018-12-08 10:56:21 +0100

    updated log

--- git-set-upstream.sh
+++ git-set-upstream.sh
@@ -2,6 +2,6 @@
 # -*- coding: UTF-8 -*-
 ## helper script to 'git push --set-upstream' current branch
 current=$(git rev-parse --abbrev-ref HEAD)
-echo git push --set-upstream origin ${current}? CTLR-C to interrupt
+echo "git push --set-upstream origin ${current}? (press ENTER to accept)"
 read
 git push --set-upstream origin ${current}
