374d0975a9b432d2b8bc168f001e5425b8c4e57f
Carlo Lobrano <c.lobrano@gmail.com>
2019-04-12 23:53:42 +0200

    launcher mockup: added badge

--- launcher-progress.py
+++ launcher-progress.py
@@ -8,6 +8,9 @@ loop = GObject.MainLoop()
 
 launcher = Unity.LauncherEntry.get_for_desktop_id ("update-manager.desktop")
 
+launcher.set_property("count", 1241201)
+launcher.set_property("count_visible", True)
+
 i = 0.0
 def progressbar():
     global i
