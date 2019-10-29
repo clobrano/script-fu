bf69c05b005a799e38a50579722aa1b784737055
Carlo Lobrano <c.lobrano@gmail.com>
2019-10-29 15:00:51 +0100

    flatpak: add setup script

---
+++ flatpak-setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+set -x
+sudo apt install -y flatpak
+sudo apt install -y gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+set +x
+echo restart the system
+
+
+
