df80b9bc0eadb64475890e29d960eb4bc802c2f6
Carlo Lobrano <c.lobrano@gmail.com>
2020-03-05 11:07:02 +0100

    usb_reset: big rework
    - add argparse
    - better error handling
    - lint with Black

--- usb_reset.py
+++ usb_reset.py
@@ -1,24 +1,56 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # vi: set ft=python :
+"""
+Reset USB connected device
+"""
 import os
 import sys
 from subprocess import Popen, PIPE
 import fcntl
+import argparse
+
 USBDEVFS_RESET = 21780
 
-if __name__ == '__main__':
-    driver = sys.argv[-1]
-    print("resetting driver: %s" % driver)
-    input()
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser(usage=__doc__)
+    PARSER.add_argument("target")
+    OPTS = PARSER.parse_args()
+
+    print("looking for device '%s'" % OPTS.target)
 
     try:
-        lsusb_out = Popen('lsusb | grep -i %s' % driver, shell=True, bufsize=64, stdin=PIPE, stdout=PIPE, close_fds=True).stdout.read().strip().split()
-        bus = lsusb_out[1]
-        device = lsusb_out[3][:-1]
-        print('bus: %s, device %s' % (bus, device))
-        f = open('/dev/bus/usb/%s/%s' % (bus, device), 'w', os.O_WRONLY)
-        fcntl.ioctl(f, USBDEVFS_RESET, 0)
-    except Exception, msg:
-        print("failed to reset device %s: %s" % (driver, msg))
+        LSUSB_OUT = (
+            Popen(
+                "lsusb | grep -i '%s'" % OPTS.target,
+                shell=True,
+                bufsize=64,
+                stdin=PIPE,
+                stdout=PIPE,
+                close_fds=True,
+            )
+            .stdout.read()
+            .strip()
+            .decode()
+        )
+        if not LSUSB_OUT:
+            print("cannot find any device")
+            sys.exit(0)
+        print("found: %s" % LSUSB_OUT)
+        LSUSB_OUT = LSUSB_OUT.split()
+        try:
+            BUS = str(LSUSB_OUT[1])
+            DEVICE = LSUSB_OUT[3][:-1]
+        except IndexError as err:
+            print("cannot get BUS and/or Device numbers from %s" % LSUSB_OUT)
+            sys.exit(1)
 
+        PATH = r"/dev/bus/usb/%s/%s" % (BUS, DEVICE)
+
+        print("about to reset device at path %s" % PATH)
+        input("press ENTER to continue")
+
+        DEVFILE = open(PATH, "w", os.O_WRONLY)
+        fcntl.ioctl(DEVFILE, USBDEVFS_RESET, 0)
+    except Exception as msg:
+        print("failed to reset device %s: %s" % (DEVICE, msg))
