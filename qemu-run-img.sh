162dca654989e466c7d843c72ca369a3d46a950f
Carlo Lobrano <carlo.lobrano@telit.com>
2021-08-30 17:03:18 +0200

    Renamed script

--- qemu-run-img.sh
+++ qemu-run-img.sh
@@ -3,9 +3,17 @@
 # This script runs QEMU using the ISO image (with the ISO image kernel)
 
 ISO_PATH="$1"
+shift
 MEM="4G"
-VID=0x1bc7
-PID=0x1040
+# VENDOR,PRODUCT ID pairs to pass to the VM in the format "0xABCD:0xEFGH 0xILMN:0xOPQR ..."
+USB_ID_LIST=( "$@" )
+usbpassthroug=""
+for id in "${USB_ID_LIST[@]}"; do
+    echo $id
+    VID=`echo $id | cut -d":" -f1`
+    PID=`echo $id | cut -d":" -f2`
+    usbpassthroug+="-usb -device usb-host,vendorid=$VID,productid=$PID "
+done
 
 [[ ! -f ${ISO_PATH} ]] && echo "Could not find ${ISO_PATH} disk file" && exit 1
 
@@ -20,5 +28,6 @@ sudo qemu-system-x86_64 \
     -cpu host \
     -m ${MEM} \
     -enable-kvm \
-    -usb -device usb-host,productid=0xd00d,vendorid=0x18d1 \
-    -usb -device usb-host,productid=$PID,vendorid=$VID
+    -net nic -net user,hostfwd=tcp::2222-:22 \
+    -net nic \
+    $usbpassthroug
