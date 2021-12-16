c91ee8d12462c2f00c774e5598a2e8639367f5cf
Carlo Lobrano <carlo.lobrano@telit.com>
2021-12-16 17:50:02 +0100

    qemu: improve qemu-install-os with CLInt

--- qemu-install-os.sh
+++ qemu-install-os.sh
@@ -1,14 +1,50 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
+## Call qemu to install an ISO image into a IMG disk
+## options
+##      -d, --disk <path>  Path to the IMG file (see qemu-create-image.sh)
+##      -i, --iso <path> Path to the ISO file
 
-# The ISO image of the OS to be installed
-ISO_PATH=$1
-# The disk IMG file to install the OS into
-IMAGE_PATH=$2
+# CLInt GENERATED_CODE: start
+
+# No-arguments is not allowed
+[ $# -eq 0 ] && sed -ne 's/^## \(.*\)/\1/p' $0 && exit 1
+
+# Converting long-options into short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+"--disk") set -- "$@" "-d";;
+"--iso") set -- "$@" "-i";;
+  *) set -- "$@" "$arg"
+  esac
+done
+
+function print_illegal() {
+    echo Unexpected flag in command line \"$@\"
+}
+
+# Parsing flags and arguments
+while getopts 'hd:i:' OPT; do
+    case $OPT in
+        h) sed -ne 's/^## \(.*\)/\1/p' $0
+           exit 1 ;;
+        d) _disk=$OPTARG ;;
+        i) _iso=$OPTARG ;;
+        \?) print_illegal $@ >&2;
+            echo "---"
+            sed -ne 's/^## \(.*\)/\1/p' $0
+            exit 1
+            ;;
+    esac
+done
+# CLInt GENERATED_CODE: end
+
+
 MEM=4G
 set -u
 
-[[ ! -f ${ISO_PATH} ]] && echo "Could not find '${ISO_PATH}' OS image file" && exit 1
-[[ ! -f ${IMAGE_PATH} ]] && echo "Could not find ${IMAGE_PATH} disk file" && exit 1
+[[ ! -f ${_iso} ]] && echo "Could not find '${_iso}' OS image file" && exit 1
+[[ ! -f ${_disk} ]] && echo "Could not find ${_disk} disk file" && exit 1
 
-qemu-system-x86_64 -cdrom ${ISO_PATH} ${IMAGE_PATH} -m ${MEM} -enable-kvm
+qemu-system-x86_64 -cdrom ${_iso} ${_disk} -m ${MEM} -enable-kvm
