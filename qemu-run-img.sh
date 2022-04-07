ecb87d297b26940e289d64fe3b95ebc9865f9149
Carlo Lobrano <c.lobrano@gmail.com>
2022-04-07 12:05:12 +0200

    qemu:aarch64: replace home folder with /mnt/qemu_shared

--- qemu-run-img.sh
+++ qemu-run-img.sh
@@ -5,6 +5,6 @@ IMG=$1
 
 qemu-system-aarch64 -nographic -machine virt,gic-version=max -m 2G -cpu max -smp 4 \
     -netdev user,id=vnet,hostfwd=:127.0.0.1:0-:22 -device virtio-net-pci,netdev=vnet \
-    -net nic -net user,smb=/home/carlo \
+    -net nic -net user,smb=/mnt/qemu_shared \
     -drive file=${IMG},if=none,id=drive0,cache=writeback -device virtio-blk,drive=drive0,bootindex=0 \
     -drive file=flash0.img,format=raw,if=pflash -drive file=flash1.img,format=raw,if=pflash
