58b02dc6ce2e331f0a8187ba923c77cc7dfa9b6b
Carlo Lobrano <c.lobrano@gmail.com>
2022-04-11 00:14:12 +0200

    tdd:modemmanager: update test list

--- modemmanager-tdd.sh
+++ modemmanager-tdd.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
 
-testlist=" test-common-helpers test-pco test-charsets test-error-helpers test-modem-helpers test-sms-part-3gpp test-sms-part-cdma test-shared-telit test-plugin-generic test-keyfiles "
-
+testlist="test-common-helpers test-pco test-charsets test-error-helpers test-modem-helpers test-sms-part-3gpp test-sms-part-cdma test-shared-telit test-plugin-generic test-keyfiles"
+testlist="test-qcdm test-common-helpers test-pco test-at-serial-port test-charsets test-error-helpers test-kernel-device-helpers test-modem-helpers test-sms-part-3gpp test-sms-part-cdma test-udev-rules test-qcdm-serial-port test-modem-helpers-qmi test-shared-icera test-shared-sierra test-shared-telit test-shared-xmm test-plugin-altair-lte test-plugin-cinterion test-plugin-generic test-plugin-huawei test-plugin-linktop test-plugin-ericsson-mbm test-plugin-quectel test-plugin-simtech test-plugin-thuraya test-plugin-ublox test-udev-rules test-keyfiles"
 
 pushd ~/workspace/telit/ModemManager
 
