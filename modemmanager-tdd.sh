c36c8efbc4da8c62b56ca4dd0809ed9be3846521
Carlo Lobrano <carlo.lobrano@telit.com>
2022-04-13 10:45:27 +0200

    tdd:modemmanager: updated script to avoid using entr directly

--- modemmanager-tdd.sh
+++ modemmanager-tdd.sh
@@ -4,10 +4,5 @@
 testlist="test-common-helpers test-pco test-charsets test-error-helpers test-modem-helpers test-sms-part-3gpp test-sms-part-cdma test-shared-telit test-plugin-generic test-keyfiles"
 testlist="test-qcdm test-common-helpers test-pco test-at-serial-port test-charsets test-error-helpers test-kernel-device-helpers test-modem-helpers test-sms-part-3gpp test-sms-part-cdma test-udev-rules test-qcdm-serial-port test-modem-helpers-qmi test-shared-icera test-shared-sierra test-shared-telit test-shared-xmm test-plugin-altair-lte test-plugin-cinterion test-plugin-generic test-plugin-huawei test-plugin-linktop test-plugin-ericsson-mbm test-plugin-quectel test-plugin-simtech test-plugin-thuraya test-plugin-ublox test-udev-rules test-keyfiles"
 
-pushd ~/workspace/telit/ModemManager
+sudo meson test -C build ${testlist}
 
-find . \( -iname "*.h" -or -iname "*.c" \) | \
-    entr -c tmux-tdd.sh --name "ModemManager" --run "sudo meson test -C build ${testlist}" --type "meson"
-popd
-
-tmux-tdd.sh --clean
