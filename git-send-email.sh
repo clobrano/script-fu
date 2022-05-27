dc49d410e8a8f4cfe2a10b167880862bd5dcb4ad
Carlo Lobrano <c.lobrano@gmail.com>
2022-05-27 10:32:43 +0200

    git-send-email: updated usage message

--- git-send-email.sh
+++ git-send-email.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
+
+echo [+] the following is the command to configure send mail.
+echo [+] if you want to sent the last N commits as patches, append "-N" to the command
+echo [+] you should also able to pass the *.patch file, but not checked yet
+
 if [[ $1 = "usb-serial" ]]; then
 echo "git send-email --annotate \
 --to=\"Johan Hovold <johan@kernel.org>\" \
 --to=\"Greg Kroah-Hartman <gregkh@linuxfoundation.org>\" \
 --cc=\"linux-usb@vger.kernel.org\" \
-$1"
+"
 fi
 
 if [[ $1 = "net" ]]; then
@@ -15,6 +20,6 @@ echo "git send-email --annotate \
 --to=\"Jakub Kicinski <kuba@kernel.org>\" \
 --cc=\"netdev@vger.kernel.org\" \
 --cc=\"linux-usb@vger.kernel.org\" \
-$1"
+"
 fi
  
