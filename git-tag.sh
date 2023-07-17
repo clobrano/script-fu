5915fdd2f5e6114b30bd5221184517977cedffe9
Carlo Lobrano <c.lobrano@gmail.com>
2023-07-17 11:46:09 +0200

    Allow to select the remote where to push the tag to
    - add new --remote flag to pass the remote name
    
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- git-tag.sh
+++ git-tag.sh
@@ -6,14 +6,18 @@
 ##      git-tagcomplete.sh --tag <version> --message <message> [--branch]
 ##      git-tagcomplete.sh --delete --tag <version>
 ## options:
+##      -r, --remote <text>        Name of the remote (e.g. origin or upstream) [default: origin]
 ##      -t, --tag <version>        Tag version (e.g. v1.0.0)
 ##      -m, --message <message>    Quoted annotation message
 ##      -b, --branch <branchname>  Branch name target of the tag
 ##      -c, --commit <commit>      Specific tag to commit
 ##      -d, --delete               Delete given tag both remotely and locally
-##      -r, --dryrun               Just shows the commands used to create/delete the tag
+##      -n, --dryrun               Just shows the commands used to create/delete the tag
 
-# GENERATED_CODE: start
+# CLInt GENERATED_CODE: start
+# info: https://github.com/clobrano/CLInt.git
+# Default values
+_remote=origin
 
 # No-arguments is not allowed
 [ $# -eq 0 ] && sed -ne 's/^## \(.*\)/\1/p' $0 && exit 1
@@ -22,12 +26,13 @@
 for arg in "$@"; do
   shift
   case "$arg" in
+"--remote") set -- "$@" "-r";;
 "--tag") set -- "$@" "-t";;
 "--message") set -- "$@" "-m";;
 "--branch") set -- "$@" "-b";;
 "--commit") set -- "$@" "-c";;
 "--delete") set -- "$@" "-d";;
-"--dryrun") set -- "$@" "-r";;
+"--dryrun") set -- "$@" "-n";;
   *) set -- "$@" "$arg"
   esac
 done
@@ -37,12 +42,13 @@ function print_illegal() {
 }
 
 # Parsing flags and arguments
-while getopts 'hdrt:m:b:c:' OPT; do
+while getopts 'hdnr:t:m:b:c:' OPT; do
     case $OPT in
         h) sed -ne 's/^## \(.*\)/\1/p' $0
            exit 1 ;;
         d) _delete=1 ;;
-        r) _dryrun=1 ;;
+        n) _dryrun=1 ;;
+        r) _remote=$OPTARG ;;
         t) _tag=$OPTARG ;;
         m) _message=$OPTARG ;;
         b) _branch=$OPTARG ;;
@@ -54,7 +60,8 @@ while getopts 'hdrt:m:b:c:' OPT; do
             ;;
     esac
 done
-# GENERATED_CODE: end
+# CLInt GENERATED_CODE: end
+
 
 dryrun=''
 [ ! -z $_dryrun ] && dryrun=echo && echo dry run: commands will have no effect
@@ -62,24 +69,25 @@ dryrun=''
 set -e
 if [ -z $_delete ]; then
     if [ -z "$_commit" ]; then
-        echo add tag "$_tag"? [press ENTER to continue]
-    else
-        echo add tag "$_tag" to the following commit?
-        echo
-        git log "$_commit" -1
-        echo [press ENTER to continue]
+        _commit=HEAD
     fi
+
+    echo add tag "$_tag" to $_remote for the following commit?
+    echo
+    git log "$_commit" -1
+    echo [press ENTER to continue]
+
     read
     [ ! -z "$_message" ] && annotation="-m $_message" || annotation=
     set -x
     $dryrun git tag -a $_tag $annotation $_commit
-    $dryrun git push origin $_tag
+    $dryrun git push $_remote $_tag
     set +x
 else
-    echo are you sure to delete tag "$_tag" [press ENTER to continue]?
+    echo are you sure to delete tag "$_tag" from $_remote [press ENTER to continue]?
     read
     set -x +e
-    $dryrun git push --delete origin $_tag
+    $dryrun git push --delete $_remote $_tag
     $dryrun git tag --delete $_tag
     set +x
 fi
