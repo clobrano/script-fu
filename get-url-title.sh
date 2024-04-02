c85a15b253813b1f3fe98a14408975f0ef105b19
Carlo Lobrano <c.lobrano@gmail.com>
2024-04-02 11:30:05 +0200

    get-url-title: get url from clipboard if input is empty
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- get-url-title.sh
+++ get-url-title.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
-wget -qO- "$@" | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si' | recode html..ascii 2>/dev/null
+
+# Get URL from input or clipboard if input is empty
+if [ -z "$1" ]; then
+    url=$(xclip -o)
+else
+    url=$1
+fi
+
+wget -qO- "$url" | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si' | recode html..ascii 2>/dev/null
