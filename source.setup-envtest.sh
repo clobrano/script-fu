f2b6f51bd39180050603a9cbe2eb60c7f9004b26
Carlo Lobrano <c.lobrano@gmail.com>
2024-07-27 12:13:42 +0200

    setup envtest improvement

--- source.setup-envtest.sh
+++ source.setup-envtest.sh
@@ -5,11 +5,12 @@ if [[ -f ./go.mod ]] then
   kubectl_location=$(find -type f -name kubectl -exec realpath {} \;)
   # exit error if not found or too many found
   if [[ -z "${kubectl_location}" ]]; then
-    echo "kubectl not found" >&2
+    echo "[!] kubectl not found" >&2
   elif [[ $(echo "${kubectl_location}" | wc -l) -gt 1 ]]; then
-    echo "multiple kubectl found" >&2
+    echo "[!] multiple kubectl found" >&2
+    echo ${kubectl_location}
   else
     export KUBEBUILDER_ASSETS=$(dirname "${kubectl_location}")
-    echo "KUBEBUILDER_ASSETS set to ${KUBEBUILDER_ASSETS}"
+    echo "[+] KUBEBUILDER_ASSETS set to ${KUBEBUILDER_ASSETS}"
   fi
 fi
