559bbad8f2231c4eb94b5d2bf3bf2b8367262379
Carlo Lobrano <c.lobrano@gmail.com>
2024-08-29 09:46:07 +0200

    shortcut
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

---
+++ key.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+ENCRIPTED_FILE=$1
+ACCOUNT=$2
+
+VALUE=$(gpg --quiet --batch --decrypt $ENCRIPTED_FILE | ykman oath accounts code $ACCOUNT | awk '{print $3}')
+code=$?
+if [ $code -ne 0 ]; then
+    notify-send --app-name "YKMAN" -u critical "Yubikey error" "$code"
+else
+    echo -n $VALUE | wl-copy
+    notify-send --app-name "YKMAN" -i dialog-information "Yubikey" "$VALUE"
+fi
