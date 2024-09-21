129a3d7814889a0dec721c6415bcddf371fbe0e6
Carlo Lobrano <c.lobrano@gmail.com>
2024-09-21 10:12:24 +0200

    start-a-day: add reminder to commit daily updates

--- start-a-day.sh
+++ start-a-day.sh
@@ -7,10 +7,12 @@ TASK="${TASK} rc.color=off"
 
 # Set the required hours of work (included 1h for lunch) from input or default to 9
 required_hours=${1:-9}
+commit_daily_update=$(( $required_hours - 1 ))
 TODAY_REM=~/.config/rem/rem.$(date +%F)
 
 if [[ ! -f ${TODAY_REM} ]]; then
-    echo "$(date +"%b/%d/%Y %H:%M") start of the day" >> ~/.config/rem/rem.$(date +%F)
+    echo "$(date +"%b/%d/%Y %H:%M") start of the day" > ~/.config/rem/rem.$(date +%F)
+    rem commit daily updates in:${commit_daily_update} hours
     rem call it a day in:${required_hours} hours
 
     echo `date +%H:%M` > $HOME/.productive-time-started
@@ -19,17 +21,23 @@ if [[ ! -f ${TODAY_REM} ]]; then
     lets do morning review
 fi
 
+command -v ${TASK} >/dev/null
+if [[ $? -ne 0 ]]; then
+    exit 0
+fi
+
+echo ""
 echo "## Quarter Goals"
 ${TASK} project:QC +PENDING
 
 
+echo ""
 echo "## NEXT (overdue/today/High)"
-${TASK} "(+OVERDUE +TODAY) or priority:H"
+${TASK} "+OVERDUE or +TODAY or priority:H -COMPLETE"
 
 echo ""
 echo "## THIS WEEK"
 ${TASK} +PENDING due.after:sow due.before:eow
-
 
 echo ""
 echo "## ACTIVE"
