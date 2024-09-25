49e09f2804204a9d5c9072ccb67fe8006daa2fb2
Carlo Lobrano <c.lobrano@gmail.com>
2024-09-25 14:48:07 +0200

    readitlater: add support for youtube playlist and improved responsiveness
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- readitlater.sh
+++ readitlater.sh
@@ -39,13 +39,13 @@ check_duplicate() {
     local url=$1
     grep "$url" ${ORG_FILEPATH} >/dev/null
     if [ $? -eq 0 ]; then
-        $WARNING "$title: already in ReadItLater"
+        $WARNING "Already in ReadItLater"
         return 1
     fi
     for archive in ${ORG_ARCHIVE_FILEPATH[@]}; do
         grep "$url" $archive >/dev/null
         if [ $? -eq 0 ]; then
-            $WARNING "$title: already in ReadItLater Archive"
+            $WARNING "Already in ReadItLater Archive"
             return 1
         fi
     done
@@ -75,30 +75,42 @@ calculate_reading_time() {
 process_youtube() {
     url=$1
 
-    info=$(yt-dlp --get-title --get-duration "$url")
-    title=$(echo "$info" | sed -n '1p')
-    duration=$(echo "$info" | sed -n '2p')
-
     check_duplicate "$url"
     if [ $? -eq 1 ]; then
         return 0
     fi
 
-    # Convert duration to seconds
-    duration_seconds=$(echo "$duration" | awk -F: '{ if (NF==3) { print ($1 * 3600) + ($2 * 60) + $3 } else if (NF==2) { print ($1 * 60) + $2 } else { print $1 } }')
-    duration_minutes=$((duration_seconds / 60))
+    more_tags=`get_tags`
 
-    if [ "$duration_minutes" -le 10 ]; then
-        duration_tag="short"
-    elif [ "$duration_minutes" -le 20 ]; then
-        duration_tag="mid"
+    if [[ "$url" =~ "playlist" ]]; then
+        title=$(yt-dlp --skip-download --print playlist_title "$url" | uniq)
+        title="$title playlist"
     else
+        info=$(yt-dlp --get-title --get-duration "$url")
+        title=$(echo "$info" | sed -n '1p')
+        duration=$(echo "$info" | sed -n '2p')
+    fi
+
+    if [[ -z $duration ]]; then
+        # right now only the playlists don't have duration, so the tag long is appropriate
+        duration="0"
         duration_tag="long"
+    else
+        # Convert duration to seconds
+        duration_seconds=$(echo "$duration" | awk -F: '{ if (NF==3) { print ($1 * 3600) + ($2 * 60) + $3 } else if (NF==2) { print ($1 * 60) + $2 } else { print $1 } }')
+        duration_minutes=$((duration_seconds / 60))
+
+        if [ "$duration_minutes" -le 10 ]; then
+            duration_tag="short"
+        elif [ "$duration_minutes" -le 20 ]; then
+            duration_tag="mid"
+        else
+            duration_tag="long"
+        fi
     fi
 
     creation_date=`date +%F`
 
-    more_tags=`get_tags`
 
     echo -e "* $title :video:$duration_tag:$more_tags\n  $url\n  created: [${creation_date}]\n  duration: $duration" >> ${ORG_FILEPATH}
     $NOTIFY "$title saved"
@@ -108,13 +120,12 @@ process_youtube() {
 process_webpage() {
     url=$1
 
-    title=$(wget -q -O - "$url" | tr "\n" " " | sed 's|.*<title>\([^<]*\).*</head>.*|\1|;s|^\s*||;s|\s*$||')
-    grep "$url" ${ORG_FILEPATH} >/dev/null
-    if [ $? -eq 0 ]; then
-        $NOTIFY "Skip $title, as it is already in ReadItLater"
+    check_duplicate "$url"
+    if [ $? -eq 1 ]; then
         return 0
     fi
 
+    title=$(wget -q -O - "$url" | tr "\n" " " | sed 's|.*<title>\([^<]*\).*</head>.*|\1|;s|^\s*||;s|\s*$||')
     content=$(curl -sL "$url" | html2text)
     reading_info=$(calculate_reading_time "$content")
     reading_time=$(echo "$reading_info" | awk '{print $1}')
