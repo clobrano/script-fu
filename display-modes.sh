da2c674393ba7c9c3bea6bbae7cbbc087920e29a
Carlo Lobrano <c.lobrano@gmail.com>
2024-10-19 17:39:43 +0200

    display-modes: create display yaml if it doesn't exist

--- display-modes.sh
+++ display-modes.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
-#options:
-  #gamma-values:
-    #180: ddcutil setvcp 0x72 0x50
-    #200: ddcutil setvcp 0x72 0x64
-    #220: ddcutil setvcp 0x72 0x78
-    #240: ddcutil setvcp 0x72 0x8c
-    #260: ddcutil setvcp 0x72 0xa0
-  #input-source:
-    #display-port: ddcutil setvcp 0x60 0x0f
-    #hdmi: ddcutil setvcp 0x60 0x10
-    #usbc: ddcutil setvcp 0x60 0x11
-  #display-modes:
-    #deactivated: ddcutil setvcp 0xdc 0x00
-    #low-blue-mode: ddcutil setvcp 0xdc 0x0b
+
+
+if [[ ! -f ~/.philips-display.yaml ]]; then
+    cat << EOF > ~/.philips-display.yaml
+options:
+  gamma-values:
+    180: ddcutil setvcp 0x72 0x50
+    200: ddcutil setvcp 0x72 0x64
+    220: ddcutil setvcp 0x72 0x78
+    240: ddcutil setvcp 0x72 0x8c
+    260: ddcutil setvcp 0x72 0xa0
+  input-source:
+    display-port: ddcutil setvcp 0x60 0x0f
+    hdmi: ddcutil setvcp 0x60 0x10
+    usbc: ddcutil setvcp 0x60 0x11
+  display-modes:
+    deactivated: ddcutil setvcp 0xdc 0x00
+    low-blue-mode: ddcutil setvcp 0xdc 0x0b
+EOF
+fi
 
 option=$(yq eval ".options | keys" ~/.philips-display.yaml | awk '{print $2}' | fzf)
 value=$(yq eval ".options.${option} | keys" ~/.philips-display.yaml | awk '{print $2}' | fzf)
