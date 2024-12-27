f4a4f940b7d823d6a5d47e35c3ef531efdad783b
Carlo Lobrano <c.lobrano@gmail.com>
2024-12-27 16:02:56 +0100

    taskwarrior-quick-task: add notification
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- taskwarrior-quick-task.sh
+++ taskwarrior-quick-task.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
-
 command -v kdialog > /dev/null
 if [ $? -eq 0 ]; then
     task=`kdialog --geometry 600x100+200+200 \
@@ -11,10 +10,29 @@ fi
 command -v termux-setup-storage > /dev/null
 if [ $? -eq 0 ]; then
     task=`termux-dialog text -t "QuickTask" -i "Buy some milk" | jq -r .text`
+fi
 
+# Default notification system is stdout
+NOTIFY="echo"
+WARNING="echo [!]"
+
+command -v notify-send > /dev/null
+if [ $? -eq 0 ]; then
+    NOTIFY="notify-send --app-name QuickTask -i dialog-information"
+    WARNING="notify-send --app-name QuickTask -i dialog-information"
+fi
+
+command -v termux-setup-storage > /dev/null
+if [ $? -eq 0 ]; then
+    NOTIFY="termux-notification --content"
+    WARNING="termux-notification --content"
 fi
 
 if [ -n "$task" ]; then
-    task add "$task" +inbox
+    out=$(task add "$task" +inbox)
+    if [ $? -eq 0 ]; then
+        $NOTIFY "$out"
+    else
+        $WARNING "$out"
+    fi
 fi
-
