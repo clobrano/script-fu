ac39401305597c84b2f064034aa4faf5bdc3a40f
Carlo Lobrano <c.lobrano@gmail.com>
2024-12-28 16:01:21 +0100

    add termux quick next script
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

---
+++ taskwarrior-quick-next.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+TERMUX_CUSTOM=""
+command -v termux-setup-storage > /dev/null
+if [ $? -eq 0 ]; then
+    TERMUX_CUSTOM="rc.data.location=$HOME/storage/documents/Me/Taskwarrior"
+fi
+out=$(task $TERMUX_CUSTOM +PENDING \(+OVERDUE or +TODAY or priority:H or +ACTIVE\) sl)
+
+command -v kdialog > /dev/null
+if [ $? -eq 0 ]; then
+    kdialog --geometry 600x100+200+200 \
+        --font Monospace \
+        --title Next \
+        --msgbox "$out"
+fi
+
+command -v termux-setup-storage > /dev/null
+if [ $? -eq 0 ]; then
+    termux-notification --content "$out"
+fi
+
+
