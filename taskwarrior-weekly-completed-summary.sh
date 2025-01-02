d7e960043ca15906669d8a64dd02b0b8d1eab749
Carlo Lobrano <c.lobrano@gmail.com>
2025-01-02 12:04:14 +0100

    taskwarrior: add completed task weekly summary script

---
+++ taskwarrior-weekly-completed-summary.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+TERMUX_CUSTOM=""
+command -v termux-setup-storage > /dev/null
+if [ $? -eq 0 ]; then
+    TERMUX_CUSTOM="rc.data.location=$HOME/storage/documents/Me/Taskwarrior"
+fi
+
+task $TERMUX_CUSTOM weekly_completed
+task $TERMUX_CUSTOM status:completed end.after:sow export | jq -r '.[] | select(.project != null) | .project' | sort | uniq -c | awk '{printf "%-20s %d\n", $2, $1}'
