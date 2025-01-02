4e3077fcbf4969a436b5bc5cbd3c1bc8ba28aee9
Carlo Lobrano <c.lobrano@gmail.com>
2025-01-02 11:40:13 +0100

    key: warning popup if something goes wrong

--- key.sh
+++ key.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
+set -o pipefail
 ENCRIPTED_FILE=$1
 ACCOUNT=$2
 
 VALUE=$(kdialog --title "Password" --password "Input Yubikey password" | ykman oath accounts code $ACCOUNT | awk '{print $3}')
-code=$?
-if [ $code -ne 0 ]; then
-    notify-send --app-name "YKMAN" -u critical "Yubikey error" "$code"
+rc=$?
+if [ $rc -ne 0 ]; then
+    notify-send --app-name "YKMAN" -u critical "Yubikey error" "could not get password (error $rc)"
 else
     echo -n $VALUE | wl-copy
     notify-send --app-name "YKMAN" -i dialog-information "Yubikey" "$VALUE"
