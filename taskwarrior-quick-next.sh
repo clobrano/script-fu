2d76012ddc38ad31616a8b18cf601ad0f326b82d
Carlo Lobrano <c.lobrano@gmail.com>
2025-01-08 10:36:12 +0100

    quick-task: remove main and book from list

--- taskwarrior-quick-next.sh
+++ taskwarrior-quick-next.sh
@@ -5,12 +5,11 @@ command -v termux-setup-storage > /dev/null
 if [ $? -eq 0 ]; then
     TERMUX_CUSTOM="rc.data.location=$HOME/storage/documents/Me/Taskwarrior"
 fi
-out=$(task $TERMUX_CUSTOM +PENDING \(+OVERDUE or +TODAY or priority:H or +ACTIVE\) sl)
+out=$(task $TERMUX_CUSTOM +PENDING \(+OVERDUE or +TODAY or priority:H or +ACTIVE\) tag.not:book tag.not:head sl)
 
 command -v kdialog > /dev/null
 if [ $? -eq 0 ]; then
     kdialog --geometry 600x100+200+200 \
-        --font Monospace \
         --title Next \
         --msgbox "$out"
 fi
