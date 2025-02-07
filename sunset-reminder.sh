4c5048dec6d18bc74d6a2129ff462694fecb274c
Carlo Lobrano <c.lobrano@gmail.com>
2025-02-07 11:44:33 +0100

    Add sunset reminder
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

---
+++ sunset-reminder.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+
+API_JSON="/tmp/sunset.json"
+
+NOTIFYSEND_ARGS=(--app-name "Sunset reminder" -i dialog-information)
+INFO="notify-send ${NOTIFYSEND_ARGS[*]}"
+NOTIFYSEND_ARGS=(--app-name "Sunset reminder" -i dialog-warning -u critical)
+WARNING="notify-send ${NOTIFYSEND_ARGS[*]}"
+
+#curl "https://api.sunrise-sunset.org/json?lat=39.242901&lng=-9.195257&formatted=0" > "$API_JSON"
+
+if STATUS=$(jq -r .status "$API_JSON"); then
+    if [ "$STATUS" != "OK" ]; then
+        $WARNING "[!] could not get sunset data: status $STATUS"
+        exit 1
+    fi
+fi
+
+set -x
+SUNSET=$(jq -r .results.sunset "$API_JSON")
+# @2025-02-07 seems that the timezone is wrong. It says UTC but,
+# but the expected sunset time looks right for CET instead.
+# For now, I'll ignore the timezone
+SUNSET=$(echo "$SUNSET" | cut -d'+' -f1)
+$INFO "Sunset attended for $(date -d "$SUNSET $TZID" +"%F %H:%M")"
+
+TIME=$(date -d "$SUNSET" +"%H:%M")
+rem Look at the sunset at:"$TIME"
