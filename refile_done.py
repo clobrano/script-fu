29a3cfae99d9096d49f656870c93198f32154ca2
Carlo Lobrano <c.lobrano@gmail.com>
2025-05-01 18:51:13 +0200

    fix refile_done

--- refile_done.py
+++ refile_done.py
@@ -1,60 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # vi: set ft=python :
+from shutil import move
 import argparse
 
 def refile_done_tasks(input_file, output_file):
+    tasks_to_keep = 0
+    tasks_to_archive = 0
+    in_done = False
+
     with open(input_file, 'r') as f:
         lines = f.readlines()
 
-    tasks_to_move = []
-    current_task = []
-    in_task = False
+    with open('/tmp/readitlater.org', 'w') as to_keep, open(output_file, 'a') as to_archive:
+        for line in lines:
+            if len(line) == 0:
+                continue
 
-    for line in lines:
-        if len(line) == 0:
-            continue
-
-        if line.startswith('*'):
-            # New Item: if we were in a DONE section, archive it
-            if in_task:
-                tasks_to_move.append(''.join(current_task))
-
-            in_task = False
-            current_task = []
+            if line.lstrip().startswith("* TODO"):
+                in_done = False
+                tasks_to_keep += 1
 
             if line.lstrip().startswith("* DONE"):
-                # New DONE section
-                in_task = True
-                current_task.append(line)
-        elif in_task:
-            current_task.append(line)
-        else:
-            current_task = []
+                in_done = True
+                tasks_to_archive += 1
 
-    # Adding Last DONE section, if any
-    if in_task:
-        tasks_to_move.append(''.join(current_task))
+            if in_done:
+                to_archive.write(line)
+            else:
+                to_keep.write(line)
 
-    # Archive selected tasks
-    with open(output_file, 'a') as f:
-        f.write('\n'.join(tasks_to_move))
+    move('/tmp/readitlater.org', input_file)
+    print(f"Refiling completed.")
+    print(f" {tasks_to_keep} Task(s) left in {input_file}.")
+    print(f" {tasks_to_archive} Task(s) moved in {output_file}.")
 
-    # Remove archived tasks
-    with open(input_file, 'w') as f:
-        f.write(''.join([line for line in lines if line not in ''.join(tasks_to_move)]))
-
-    print(f"Refiling completed. {len(tasks_to_move)} Task(s) moved in {output_file}.")
 
 def main():
-    # Imposta argparse per gestire gli argomenti da riga di comando
     parser = argparse.ArgumentParser(description='Refile DONE tasks from an Orgmode file to another file.')
     parser.add_argument('input_file', type=str, help='orgmode source file.')
     parser.add_argument('output_file', type=str, help='orgmode destination file.')
 
     args = parser.parse_args()
 
-    # Chiama la funzione di refiling con i file forniti dall'utente
     refile_done_tasks(args.input_file, args.output_file)
 
 if __name__ == "__main__":
