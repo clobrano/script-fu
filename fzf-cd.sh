2e90a4c89836a5e1464672f9abd889b1efc19c92
Carlo Lobrano <c.lobrano@gmail.com>
2025-05-29 08:44:49 +0200

    fuz: always edit the selected file, use C-y to copy it instead

--- fzf-cd.sh
+++ fzf-cd.sh
@@ -4,6 +4,7 @@
 
 # Set the target directory
 TARGET_DIR="$HOME/workspace"
+MAXDEPTH=3
 
 # Check if fzf is installed
 if ! command -v fzf &> /dev/null
@@ -12,18 +13,22 @@ then
     exit 1
 fi
 
-# Change directory to workspace if it exists
+# Change directory to TARGET_DIR if it exists
 if [ ! -d "$TARGET_DIR" ]; then
-  echo "Target directory $TARGET_DIR "
+  echo "Target directory $TARGET_DIR does not exist"
   exit 1
 fi
+
+# Keep pushd in place of cd, as we need to come back to the
+# original directory if something does not work in the next steps
 pushd "$TARGET_DIR" || exit 1
 
 # Use fzf to select a directory
-SELECTED_DIR=$(find . -maxdepth 5 -type d -not -path "*/.git*" | sort | \
+SELECTED_DIR=$(find . -maxdepth "$MAXDEPTH" -type d -not -path "*/.git*" | sort | \
     fzf --prompt "Search a workspace > " \
         --layout reverse \
-        --height=70%)
+        --height=70% \
+        --no-sort)
 
 # Check if a directory was selected
 if [ -z "$SELECTED_DIR" ]; then
@@ -31,6 +36,7 @@ if [ -z "$SELECTED_DIR" ]; then
     popd || exit 1
 else
     # Change directory to the selected directory
+    echo "Moving to $TARGET_DIR/$SELECTED_DIR"
     cd "$TARGET_DIR/$SELECTED_DIR" || exit 1
 fi
 
