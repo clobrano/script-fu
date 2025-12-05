e120ae454c5e1aefdcfa2945e6721b80ce9b77f2
Carlo Lobrano <c.lobrano@gmail.com>
2025-12-05 13:24:40 +0100

    letsdo: fixed taskwarrior integration

--- letsdo-taskwarrior-task.sh
+++ letsdo-taskwarrior-task.sh
@@ -8,49 +8,40 @@
 description=$*
 # default cmd uses input description for letsdo
 cmd="lets goto $description"
-# Use a regular expression to extract the hash (UUID)
-hash=$(echo "$description" | grep -oE '[a-f0-9]{8}(-[a-f0-9]{4}){3}-[a-f0-9]{12}|[a-f0-9]{8}')
-is_task=$(echo "$description" | grep -c "#W")
+# Use a regular expression to extract the hash (UUID) from rounded parentheses
+hash=$(echo "$description" | grep -oP '(?<=\()([a-f0-9]{8}(-[a-f0-9]{4}){3}-[a-f0-9]{12}|[a-f0-9]{8})(?=\))' | head -n 1)
 
-for ctx in "wk" "me"; do
-    TASK="task"
-    if [ "$ctx" = "wk" ]; then
-        TASK="task rc:~/.taskworkrc"
+TASK="task"
+if [ -n "$hash" ]; then
+    task_description=$($TASK "$hash" export | jq -r '.[0].description')
+    if [ -z "$task_description" ] || [ "$task_description" = "null" ]; then
+        echo "no taskwarrior task found, start a letsdo session with the input description"
+        $cmd
+        exit 0
     fi
 
-    if [ -n "$hash" ]; then
-        task_description=$($TASK "$hash" export | jq -r '.[0].description')
-        if [ -z "$task_description" ] || [ "$task_description" = "null" ]; then
-            continue
-        fi
-
-        project=$($TASK "$hash" export | jq -r '.[0].project')
-        if [ "$project" != "null" ]; then
-            PROJECT="@$project"
-        fi
-
-        if tw_tags=$($TASK "$hash" export | jq -r '.[0].tags'); then
-            if [ -n "$tw_tags" ] && [ "$tw_tags" != "null" ]; then
-                tw_tags=$($TASK "$hash" export | jq -r '.[0].tags[]')
-                ld_tags=""
-
-                for tw_tag in ${tw_tags}; do
-                    if [ "$tw_tags" == "meeting" ]; then
-                        continue
-                    fi
-                    ld_tags+="#$tw_tag "
-                done
-            fi
-        fi
-
-        if [ "$is_task" -eq 1 ]; then
-            description="$task_description"
-        fi
-
-        lets goto "$description" "$PROJECT" "$ld_tags" "$hash"
-        exit $?
+    project=$($TASK "$hash" export | jq -r '.[0].project')
+    if [ "$project" != "null" ]; then
+        PROJECT="@$project"
     fi
-done
 
-# no taskwarrior task found, start a letsdo session with the input description
-$cmd
+    if tw_tags=$($TASK "$hash" export | jq -r '.[0].tags'); then
+        if [ -n "$tw_tags" ] && [ "$tw_tags" != "null" ]; then
+            tw_tags=$($TASK "$hash" export | jq -r '.[0].tags[]')
+            ld_tags=""
+
+            for tw_tag in ${tw_tags}; do
+                if [ "$tw_tags" == "meeting" ]; then
+                    continue
+                fi
+                ld_tags+="#$tw_tag "
+            done
+        fi
+    fi
+
+    description="$task_description $ld_tags $PROJECT ($hash)"
+    lets goto "$description"
+    exit $?
+fi
+
+
