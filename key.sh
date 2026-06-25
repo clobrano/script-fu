d71039274d3e7acf2a6198ae2683bfa9e0c96b26
Carlo Lobrano <clobrano@clobrano-thinkpadx1carbon6th.rmtit.csb>
2026-06-25 16:23:29 +0200

    Get account from env variable

--- key.sh
+++ key.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
 set -o pipefail
-ENCRIPTED_FILE=$1
-ACCOUNT=$2
+ACCOUNT=${1:-$YUBIKEY_ACCOUNT}
+
+echo "$ACCOUNT"
+if [ -z "${ACCOUNT}" ]; then
+    notify-send --app-name "YKMAN" -u critical "Yubikey error" "YUBIKEY_ACCOUNT was not set" && exit 
+    exit 1
+fi
 
 if ! command -v ykman >/dev/null; then
     sudo dnf install -y yubikey-manager || exit 1
@@ -11,7 +16,7 @@ fi
 VALUE=$(kdialog --title "Password" --password "Input Yubikey password" | ykman oath accounts code "$ACCOUNT" | awk '{print $3}')
 rc=$?
 if [ $rc -ne 0 ]; then
-    notify-send --app-name "YKMAN" -u critical "Yubikey error" "could not get password (error $rc)"
+    notify-send --app-name "YKMAN" -u critical "Yubikey error" "Could not get password (error $rc)"
 else
     echo -n "$VALUE" | wl-copy
     notify-send --app-name "YKMAN" -i dialog-information "Yubikey" "$VALUE"
